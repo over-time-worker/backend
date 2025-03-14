470c2505eb6bef68eadb85ce1b99346a2af05ca9
Sunro1994 <gwangjulsr@gmail.com>
2025-03-14 21:58:57 +0900

    [OV-30] refactor(product) : 상품 총 수량 조회 로직 추가
    - 각 허브에서 재고 조회 후 총 재고를 조회하여 갱신하는 로직 추가

--- product/src/main/java/com/owlexpress/product/domain/service/ProductDomainServiceImpl.java
+++ product/src/main/java/com/owlexpress/product/domain/service/ProductDomainServiceImpl.java
@@ -1,7 +1,7 @@
 package com.owlexpress.product.domain.service;
 
-import com.owlexpress.product.domain.repository.ProductRepository;
 import com.owlexpress.product.domain.entity.Product;
+import com.owlexpress.product.domain.repository.ProductRepository;
 import com.owlexpress.product.presentation.dto.request.CreateProductRequestDto;
 import com.owlexpress.product.presentation.dto.request.UpdateProductDto;
 import com.owlexpress.product.presentation.dto.response.FindProductResponse;
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
@@ -35,13 +36,24 @@ public class ProductDomainServiceImpl implements ProductDomainService {
         product.setProductName(updateProductDto.getProductName());
         product.setProductPrice(updateProductDto.getProductPrice());
         product.setProductType(updateProductDto.getProductType());
+
+        //TODO :: 변경된 상품 정보를 연결된 각 허브로 FeignClient 요청을 날려서 허브 수정 API 발동!
+        // (업데이트 시키는 동안 유저가 조회하지 못하게 막아야함)
     }
 
     @Override
     public FindProductResponse find(UUID productsId) {
         Product product = getProduct(productsId);
 
-        return FindProductResponse.toDTO(product);
+        AtomicInteger totalQuantity = new AtomicInteger(1000);
+
+        product.getHubInfo().stream().forEach(
+                hubInfo ->
+                        totalQuantity.addAndGet(hubInfo.getHubProductQuantity()) //TODO :: Hub에 재고조회 API 날려서 각 재고 모아오기
+        );
+
+
+        return FindProductResponse.toDTO(product,totalQuantity);
     }
 
     private Product getProduct(UUID productsId) {
--- product/src/main/java/com/owlexpress/product/presentation/dto/response/FindProductResponse.java
+++ product/src/main/java/com/owlexpress/product/presentation/dto/response/FindProductResponse.java
@@ -7,6 +7,7 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
 public class FindProductResponse {
@@ -14,27 +15,31 @@ public class FindProductResponse {
     private String productName;
     private BigDecimal productPrice;
     private ProductType productType;
+    private int totalQuantity;
 
     @Builder
     public FindProductResponse(
             UUID productId,
             String productName,
             BigDecimal productPrice,
-            ProductType productType
+            ProductType productType,
+            int totalQuantity
     )
     {
         this.productId = productId;
         this.productName = productName;
         this.productPrice = productPrice;
         this.productType = productType;
+        this.totalQuantity = totalQuantity;
     }
 
-    public static FindProductResponse toDTO(Product product) {
+    public static FindProductResponse toDTO(Product product, AtomicInteger totalQuantity) {
         return com.owlexpress.product.presentation.dto.response.FindProductResponse.builder()
                 .productName(product.getProductName())
                 .productId(product.getProductId())
                 .productType(product.getProductType())
                 .productPrice(product.getProductPrice())
+                .totalQuantity(totalQuantity.get())
                 .build();
     }
 }
