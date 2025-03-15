e146217447a2609043ac794d7aa46499666a6198
Sunro1994 <gwangjulsr@gmail.com>
2025-03-15 21:47:28 +0900

    [OV-37] refactor(product) : 엔티티 필드 구분
    - HubInfo에 잘못 추가한 Producer필드들 다시 Product에 포함시킴

--- product/src/main/java/com/owlexpress/product/domain/entity/HubInfo.java
+++ product/src/main/java/com/owlexpress/product/domain/entity/HubInfo.java
@@ -2,7 +2,6 @@ package com.owlexpress.product.domain.entity;
 
 import com.owlexpress.product.common.BaseEntity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.UUID;
@@ -16,18 +15,8 @@ public class HubInfo extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "hub_info_id")
     private UUID hubInfoId;
-
-    @Column(name = "producer_id")
-    private UUID producerId;
-
-    @Column(name = "producer_name")
-    @Size(min = 1, max = 50)
-    private String producerName;
-
-    @Column(name = "producer_address")
-    @Size(min = 1, max = 255)
-    private String producerAddress;
 
     @Column(name = "hub_id")
     private UUID hubId;
@@ -42,18 +31,12 @@ public class HubInfo extends BaseEntity {
     @Builder
     public HubInfo(
             UUID hubInfoId,
-            UUID producerId,
-            String producerName,
-            String producerAddress,
             UUID hubId,
             Integer hubProductQuantity,
             Product product
     )
     {
         this.hubInfoId = hubInfoId;
-        this.producerId = producerId;
-        this.producerName = producerName;
-        this.producerAddress = producerAddress;
         this.hubId = hubId;
         this.hubProductQuantity = hubProductQuantity;
         this.product = product;
--- product/src/main/java/com/owlexpress/product/domain/entity/Product.java
+++ product/src/main/java/com/owlexpress/product/domain/entity/Product.java
@@ -23,26 +23,50 @@ public class Product extends BaseEntity {
     @Column(name = "product_id", nullable = false)
     private UUID productId;
 
-    @Column(name = "product_name", nullable = false)
     @Size(max = 50)
+    @Column(name = "product_name", nullable = false)
     private String productName;
 
-    @Column(name = "product_price", nullable = false)
     @Digits(integer = 13, fraction = 2)
+    @Column(name = "product_price", nullable = false)
     private BigDecimal productPrice;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "product_type", nullable = false)
     private ProductType productType;
 
+    @Column(name = "producer_id")
+    private UUID producerId;
+
+    @Size(min = 1, max = 50)
+    @Column(name = "producer_name")
+    private String producerName;
+
+    @Size(min = 1, max = 255)
+    @Column(name = "producer_address")
+    private String producerAddress;
+
     @OneToMany(mappedBy = "product")
     public Set<HubInfo> hubInfo;
 
     @Builder
-    public Product(String productName, BigDecimal productPrice, ProductType productType) {
-       this.productName = productName;
-       this.productPrice = productPrice;
-       this.productType = productType;
+    public Product(
+            String productName,
+            BigDecimal productPrice,
+            ProductType productType,
+            UUID producerId,
+            String producerName,
+            String producerAddress,
+            Set<HubInfo> hubInfo
+    )
+    {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productType = productType;
+        this.producerId = producerId;
+        this.producerName = producerName;
+        this.producerAddress = producerAddress;
+        this.hubInfo = hubInfo;
     }
 
 }
