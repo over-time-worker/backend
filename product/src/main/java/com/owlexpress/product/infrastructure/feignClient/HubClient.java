12e9915246f61e0be72104926729af257a7571da
Sunro1994 <gwangjulsr@gmail.com>
2025-03-16 18:09:05 +0900

    [OV-30] refactor(product) : Openfeign, Client Server 연결
    - OpenFeign 및 Clinet 의존성 추가
    - HubClinet 인터페이스 개발
    - ProducerClient 인터페이스 개발
    - 이후 적용할 내용 TODO로 기록

--- product/src/main/java/com/owlexpress/product/infrastructure/feignClient/HubClient.java
+++ product/src/main/java/com/owlexpress/product/infrastructure/feignClient/HubClient.java
@@ -1,12 +1,10 @@
-//package com.owlexpress.product.infrastructure.feignClient;
-//
-//import org.springframework.cloud.openfeign.FeignClient;
-//
-//@FeignClient(name = "hub-service")
-//public interface HubClient {
-//
-//    //허브 상품 재고 조회
-//
-//    //허브 상품 재고 수정
-//
-//}
+package com.owlexpress.product.infrastructure.feignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "hub-service")
+public interface HubClient {
+
+    //허브 상품 재고 조회
+    //TODO:: 허브 상품 재고 조회해서 hub-info에 등록하는 API 요청
+}
