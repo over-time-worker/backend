1431d5ad57eff368c78cdb1a0e4c2a34c539d5f4
Sunro1994 <gwangjulsr@gmail.com>
2025-03-16 17:52:04 +0900

    [OV-43] feat(producer) : 환경 설정
    - 각 패키지 생성 및 하위 필요 패키지들 구현
    - QueryDSL 의존성 추가 및 페이징 관련 Config 구현
    - 응답용 commonDTO 개발
    - BaseEntity 개발
    - GlobalExceptionAdvice 개발

--- producer/src/main/java/com/owlexpress/producer/ProducerApplication.java
+++ producer/src/main/java/com/owlexpress/producer/ProducerApplication.java
@@ -2,8 +2,14 @@ package com.owlexpress.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ProducerApplication {
 
     public static void main(String[] args) {
