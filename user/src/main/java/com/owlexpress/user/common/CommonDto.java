0107fb4900fa5d05b18313a38f75a20a36f5f710
adlaylatte <dgs2288@naver.com>
2025-03-17 01:31:19 +0900

    [OV-62] chore(user): 회원 서비스 환경설정 추가 (#65)
    - queryDSL 의존성 추가 및 환경설정
    - SpringSecurity 의존성 및 환경설정
    - JpaAuditing 환경설정
    - 전역 예외처리
    - CommonDto 생성

---
+++ user/src/main/java/com/owlexpress/user/common/CommonDto.java
@@ -0,0 +1,26 @@
+package com.owlexpress.user.common;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommonDto<T> {
+
+    private HttpStatus status;
+    private Integer code;
+    private String message;
+    private T data;
+
+    @Builder
+    public CommonDto(HttpStatus status, Integer code, String message, T data) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+}
