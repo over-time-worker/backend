5fb7da996e63a882e2647e8165db1913b67c0556
Sunro1994 <gwangjulsr@gmail.com>
2025-03-17 01:54:16 +0900

    [OV-46] refactor(producer) : dto common 패키지로 이동
    - 두 계층에서 사용하는 dto를 공통패키지에 두고 사용하여 책임 분리
    - CreateProducerRequestDto에 빠진 필드 userId추가

---
+++ producer/src/main/java/com/owlexpress/producer/common/dto/request/UpdateProductRequestDto.java
@@ -0,0 +1,56 @@
+package com.owlexpress.producer.common.dto.request;
+
+import com.owlexpress.producer.domain.entity.constant.CompanyType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class UpdateProductRequestDto {
+
+    //TODO:: 정보 수정시 모두 다시 받도록 체크?
+    @NotNull(message = "유저 ID값은 필수입니다.")
+    private Long userId;
+    @Size(min = 1, max = 20)
+    @NotNull(message = "사업자 번호는 필수값입니다.")
+    private String businessNumber;
+    @Size(min = 1, max = 20)
+    @NotNull(message = "회사명은 필수값입니다.")
+    private String companyName;
+    @NotNull(message = "회사 타입값은 필수값입니다.")
+    private CompanyType companyType;
+    @Size(min = 1, max = 255)
+    @NotNull(message = "회사 주소값은 필수값입니다.")
+    private String companyAddress;
+    @NotNull(message = "위도 값은 필수입니다.")
+    private Double latitude;
+    @NotNull(message = "경도 값은 필수입니다.")
+    private Double longitude;
+    @NotNull(message = "허브Id값은 필수값입니다.")
+    private UUID hubId;
+
+    @Builder
+    public UpdateProductRequestDto(
+            Long userId,
+            String businessNumber,
+            String companyName,
+            CompanyType companyType,
+            String companyAddress,
+            Double latitude,
+            Double longitude,
+            UUID hubId
+    ) {
+        this.userId = userId;
+        this.businessNumber = businessNumber;
+        this.companyName = companyName;
+        this.companyType = companyType;
+        this.companyAddress = companyAddress;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.hubId = hubId;
+    }
+
+}
