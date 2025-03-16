6d16f861c11ad3bd3c2819c09512f45c13ebfb9c
ggumi030 <tndus50843@gmail.com>
2025-03-16 23:57:39 +0900

    [OV-57] feat(alarm): entity 및 dto 설정 (#60)

---
+++ alarm/src/main/java/com/owl_express/alarm/application/dtos/request/AlarmCreateRequestDto.java
@@ -0,0 +1,34 @@
+package com.owl_express.alarm.application.dtos.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AlarmCreateRequestDto {
+    @NotNull(message = "[notNull:deliverId]")
+    private UUID deliverId;
+    @Size(min = 1, max = 50, message = "[size:deliverName]")
+    private String deliverName;
+    @Size(min = 1, max = 50, message = "[size:deliverPlatformId]")
+    private String deliverPlatformId;
+    @Size(min = 1, max = 50, message = "[size:platformName]")
+    private String platformName;
+    @NotNull(message = "[notNull:orderId]")
+    private UUID orderId;
+    @Size(min = 1, max = 50, message = "[size:ordererName]")
+    private String ordererName;
+    @Size(min = 1, max = 50, message = "[size:startHub]")
+    private String startHub;
+    @Size(min = 1, max = 50, message = "[size:destination]")
+    private String destination;
+    private String orderDescription;
+    @NotBlank(message = "[notBlank:departureDeadline]")
+    private String departureDeadline;
+
+}
---
+++ alarm/src/main/java/com/owl_express/alarm/domain/entity/Notification.java
@@ -0,0 +1,57 @@
+package com.owl_express.alarm.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "p_notification")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity {
+    @Id
+    @Column(name = "notification_id", length = 50)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", length = 50, nullable = false)
+    private Long userId;
+
+    @Column(name = "user_platform_id", length = 50, nullable = false)
+    private String userPlatformId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "platform_type", length = 50, nullable = false)
+    private PlatformType platformType;
+
+    @Column(name = "message", columnDefinition = "TEXT")
+    private String message;
+
+    @Column(name = "send_at")
+    private LocalDateTime sendAt;
+
+    @Column(name = "is_send", nullable = false)
+    private Boolean isSend = false;
+
+    @Column(name = "ai_id")
+    private UUID aiId;
+
+    @RequiredArgsConstructor
+    public enum PlatformType{
+        SLACK("slack");
+
+        private final String value;
+    }
+}
+
