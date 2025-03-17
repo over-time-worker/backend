c4990a6bacac4f6d93a92c235141bbee76481267
ggumi030 <tndus50843@gmail.com>
2025-03-17 18:06:10 +0900

    [OV-13]feat(alarm) : 업체 담당자 메세지 예약 기능 구현(#83)

--- alarm/src/main/java/com/owl_express/alarm/common/util/CommonUtil.java
+++ alarm/src/main/java/com/owl_express/alarm/common/util/CommonUtil.java
@@ -1,15 +1,27 @@
 package com.owl_express.alarm.common.util;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class CommonUtil {
 
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter DayDateTimeFormatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);
 
     public static String LocalDateTimetoString(LocalDateTime localDateTime) {
 
         return localDateTime == null ? null : localDateTime.format(dateTimeFormatter);
+    }
+
+    public static LocalDateTime gmtStringToDefaultLocalDateTime(String gmtDateString) {
+        ZonedDateTime gmtDateTime = ZonedDateTime.parse(gmtDateString, DayDateTimeFormatter);
+        ZonedDateTime localDateTime = gmtDateTime.withZoneSameInstant(ZoneId.systemDefault());
+
+        return localDateTime.toLocalDateTime();
+
     }
 
 }
