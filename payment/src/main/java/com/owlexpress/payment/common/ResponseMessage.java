bc466f84dac1ab8776b5fae58e0db3384cb5cd4d
cnxw4570123 <cnxw4570123@gmail.com>
2025-03-19 02:51:00 +0900

    [OV-140] chore(payment) : 결제 모듈 추가

---
+++ payment/src/main/java/com/owlexpress/payment/common/ResponseMessage.java
@@ -0,0 +1,9 @@
+package com.owlexpress.payment.common;
+
+public class ResponseMessage {
+
+    public static final String PAYMENT_CREATE_SUCESS = "결제 성공";
+    public static final String PAYMENT_CANCEL_SUCESS = "결제 취소 성공";
+    public static final String PAYMENT_DETAILS_SEARCH_SUCCESS = "결제 상세 조회 성공";
+    public static final String PAYMENT_SEARCH_BY_PRODUCT_NAME_SUCCESS = "결제 상세 조회 성공";
+}
