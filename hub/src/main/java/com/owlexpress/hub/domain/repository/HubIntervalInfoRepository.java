141215c65f125fc7730ea282309920cb1dc2edd8
Sunro1994 <gwangjulsr@gmail.com>
2025-03-19 06:18:09 +0900

    [OV-130] HubIntervalInfo Repository 개발

---
+++ hub/src/main/java/com/owlexpress/hub/domain/repository/HubIntervalInfoRepository.java
@@ -0,0 +1,7 @@
+package com.owlexpress.hub.domain.repository;
+
+import com.owlexpress.hub.domain.entity.HubIntervalInfo;
+
+public interface HubIntervalInfoRepository {
+    HubIntervalInfo save(HubIntervalInfo intervalInfo);
+}
