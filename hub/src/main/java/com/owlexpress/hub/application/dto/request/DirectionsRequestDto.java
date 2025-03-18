9aeb47faf953205d5454f004a61bf94204328254
Sunro1994 <gwangjulsr@gmail.com>
2025-03-19 06:02:32 +0900

    [OV-146] NaverAPI 송수신용 dto 개발
    - 송신시 String 반환을 위한 convertPointToString 메서드 개발
    - option은 기본 설정이 trafast(최단경로)

---
+++ hub/src/main/java/com/owlexpress/hub/application/dto/request/DirectionsRequestDto.java
@@ -0,0 +1,43 @@
+package com.owlexpress.hub.application.dto.request;
+
+import com.owlexpress.hub.common.exception.HubIntervalInfoException;
+import com.owlexpress.hub.domain.entity.Hub;
+import lombok.*;
+import org.locationtech.jts.geom.Point;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DirectionsRequestDto {
+    private String start;   // 시작 좌표 (경도, 위도)
+
+    private String goal;    // 목적지 좌표 (경도, 위도)
+    private String option;  // 경로 탐색 옵션 (기본값: fastest)
+
+    @Builder
+    public DirectionsRequestDto(
+            String start,
+            String goal,
+            String option
+    ) {
+        this.start = start;
+        this.goal = goal;
+        this.option = option != null? option: "trafast";
+    }
+
+    public static DirectionsRequestDto fromEntity(Hub startHub, Hub endHub) {
+        return DirectionsRequestDto.builder()
+                            .start(convertPointToString(startHub.getLocation()))
+                            .goal(convertPointToString(endHub.getLocation()))
+                            .option("trafast")
+                            .build();
+    }
+
+    private static String convertPointToString(Point location) {
+        if (location == null) {
+            throw new HubIntervalInfoException.LocationNotExistException();
+        }
+        return location.getX() + "," + location.getY();
+    }
+
+
+}
