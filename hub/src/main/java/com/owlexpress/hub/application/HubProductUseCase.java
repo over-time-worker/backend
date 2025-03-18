0512baf09b7a93027fbea727d9852cf07083c994
cnxw4570123 <cnxw4570123@gmail.com>
2025-03-19 00:29:41 +0900

    [OV-123] comment(hub) : 허브 상품 삭제 로직 정리
    - FeignClient 연결 시 사용할 로직 주석으로 추가

--- hub/src/main/java/com/owlexpress/hub/application/HubProductUseCase.java
+++ hub/src/main/java/com/owlexpress/hub/application/HubProductUseCase.java
@@ -1,5 +1,7 @@
 package com.owlexpress.hub.application;
 
+import com.owlexpress.hub.application.dto.response.OrderSearchResponseDto;
+import com.owlexpress.hub.application.dto.response.OrderSearchResponseDto.OrderProduct;
 import com.owlexpress.hub.common.HubHelper;
 import com.owlexpress.hub.common.exception.HubProductException.HubProductNotFoundException;
 import com.owlexpress.hub.domain.entity.Hub;
@@ -10,6 +12,8 @@ import com.owlexpress.hub.infrastructure.client.ProductClient;
 import com.owlexpress.hub.presentation.dto.request.HubProductCreateRequestDto;
 import com.owlexpress.hub.presentation.dto.request.HubProductUpdateRequestDto;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -53,12 +57,12 @@ public class HubProductUseCase {
             List<OrderSearchResponseDto> content = data.getContent();
          */
 
-
         /* TODO: <2 ~ 3>. 해당 주문들의 주문 내역에서 변경 상품이 있는지 확인
-            주문.stream()
-                .filter(주문 -> 주문.orderProducts.contains("허브 상품")) // 이 부분이 좀 쉽지 않을 듯
-                .map(주문 -> 주문.get주문_ID)
-                .forEach(주문_아이디 -> OrderClient.주문 취소(주문_아이디));
+            List<UUID> cancelOrders = content.stream()
+                .flatMap(order -> order.getOrderProducts().stream())
+                .filter(orderProduct -> orderProduct.getProductId().equals(hubProductId))
+                .map(OrderProduct::getOrderId)
+                .forEach(orderId -> orderClient.deleteOrder(orderId));
          */
 
     }
