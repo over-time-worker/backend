12e58bce28f40f5a1f668717cdc5c7d345e37cb1
cnxw4570123 <cnxw4570123@gmail.com>
2025-03-19 00:05:27 +0900

    [OV-123] feat(hub) : 허브 상품 삭제 기능 구현
    - FeignClient 연동 부분 제외 임시 구현

--- hub/src/main/java/com/owlexpress/hub/application/HubProductUseCase.java
+++ hub/src/main/java/com/owlexpress/hub/application/HubProductUseCase.java
@@ -1,11 +1,16 @@
 package com.owlexpress.hub.application;
 
 import com.owlexpress.hub.common.HubHelper;
+import com.owlexpress.hub.common.exception.HubProductException.HubProductNotFoundException;
 import com.owlexpress.hub.domain.entity.Hub;
 import com.owlexpress.hub.domain.entity.HubProduct;
 import com.owlexpress.hub.domain.repository.HubRepository;
+import com.owlexpress.hub.infrastructure.client.OrderClient;
 import com.owlexpress.hub.infrastructure.client.ProductClient;
 import com.owlexpress.hub.presentation.dto.request.HubProductCreateRequestDto;
+import com.owlexpress.hub.presentation.dto.request.HubProductUpdateRequestDto;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +22,7 @@ public class HubProductUseCase {
 
     private final HubRepository hubRepository;
     private final ProductClient productClient;
+    private final OrderClient orderClient;
 
     public HubProduct create(HubProductCreateRequestDto requestDto) {
 
@@ -29,6 +35,32 @@ public class HubProductUseCase {
         hubProduct.createdEntity(1L);
         hub.getHubProduct().add(hubProduct);
         return hubProduct;
+    }
+
+    @Transactional
+    public void delete(UUID hubProductId) {
+        HubProduct hubProduct = hubRepository.findByHubProductId(hubProductId)
+                .orElseThrow(HubProductNotFoundException::new);
+
+        // TODO: PASSPORT 에서 값 뺴오기
+        hubProduct.deleteEntity(1L);
+
+        LocalDateTime deletedAt = hubProduct.getDeletedAt();
+
+        /* TODO: 한 페이지가 끝이 아니라면, 전체 사이즈 / 사이즈(50)으로 반복 요청..?
+            1. 변경 이후 시점의 모든 주문 검색
+            PagedModel<OrderSearchResponseDto> data = orderClient.orderSearch(deletedAt, 1).getData();
+            List<OrderSearchResponseDto> content = data.getContent();
+         */
+
+
+        /* TODO: <2 ~ 3>. 해당 주문들의 주문 내역에서 변경 상품이 있는지 확인
+            주문.stream()
+                .filter(주문 -> 주문.orderProducts.contains("허브 상품")) // 이 부분이 좀 쉽지 않을 듯
+                .map(주문 -> 주문.get주문_ID)
+                .forEach(주문_아이디 -> OrderClient.주문 취소(주문_아이디));
+         */
+
     }
 
 }
--- hub/src/main/java/com/owlexpress/hub/presentation/HubProductController.java
+++ hub/src/main/java/com/owlexpress/hub/presentation/HubProductController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -97,5 +98,21 @@ public class HubProductController {
                         .build();
 
         return ResponseEntity.ok(found);
+    }
+
+    @DeleteMapping("/{hubProductId}")
+    public ResponseEntity<CommonDto<Void>> delete(
+            @PathVariable("hubProductId") UUID hubProductId
+    ) {
+        hubProductUseCase.delete(hubProductId);
+        CommonDto<Void> deleted =
+                CommonDto.<Void>builder()
+                        .status(HttpStatus.OK)
+                        .code(HttpStatus.OK.value())
+                        .message("허브 상품 삭제 완료")
+                        .data(null)
+                        .build();
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(deleted);
     }
 }
