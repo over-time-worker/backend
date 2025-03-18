b7f820a7467b2d91907913c4a30501efa6f3e01e
Sunro1994 <gwangjulsr@gmail.com>
2025-03-19 06:04:30 +0900

    [Ov-130] 모든 허브 연결하는 calculateAllHubDistances 수행하는 Initializer
    - 이후에는 수행확인 엔티티를 만들어 체크하거나 Redis로 기록 저장 예정
    
    - 서버 시작 이벤트를 감지해서 메서드 시작함

---
+++ hub/src/main/java/com/owlexpress/hub/application/HubDistanceInitializer.java
@@ -0,0 +1,18 @@
+package com.owlexpress.hub.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HubDistanceInitializer {
+
+    private final HubDistanceService hubDistanceService;
+//서버에서 한 번만실행되는 TaskChecker Entity가 필요할지도...?
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void initialize() {
+//        hubDistanceService.calculateAllHubDistances();
+//    }
+}
