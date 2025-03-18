056be9d00af647707644983279f4d073250d7003
cnxw4570123 <cnxw4570123@gmail.com>
2025-03-19 03:31:27 +0900

    [OV-145] chore(payment) : 결제 엔티티 및 리포지토리 추가

---
+++ payment/src/main/java/com/owlexpress/payment/domain/entity/Payment.java
@@ -0,0 +1,52 @@
+package com.owlexpress.payment.domain.entity;
+
+import com.owlexpress.payment.common.BaseEntity;
+import com.owlexpress.payment.domain.constant.PaymentStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment extends BaseEntity {
+
+    @Id
+    @Column(name = "payment_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID paymentId;
+
+    @Column(name = "order_id", nullable = false)
+    private UUID orderId;
+
+    @Column(name = "transaction_id", nullable = false)
+    private UUID transactionId;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_status", nullable = false)
+    private PaymentStatus paymentStatus;
+
+
+    @Builder
+    public Payment(UUID paymentId, UUID orderId, UUID transactionId, BigDecimal price,
+            PaymentStatus paymentStatus) {
+        this.paymentId = paymentId;
+        this.orderId = orderId;
+        this.transactionId = transactionId;
+        this.price = price;
+        this.paymentStatus = paymentStatus;
+    }
+}
