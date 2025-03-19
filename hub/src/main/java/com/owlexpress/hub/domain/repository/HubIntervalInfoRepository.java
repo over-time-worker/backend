c2e20344ee290a722fb39d8e5ffcd055b10ca940
Sunro1994 <gwangjulsr@gmail.com>
2025-03-19 13:57:59 +0900

    [OV-130] refactor(hubIntervalInfo) : Interval 변환 기능 구현
    - 직접 쿼리문 작성하여 Duration값을 Interval로 수정

--- hub/src/main/java/com/owlexpress/hub/domain/repository/HubIntervalInfoRepository.java
+++ hub/src/main/java/com/owlexpress/hub/domain/repository/HubIntervalInfoRepository.java
@@ -1,7 +1,16 @@
 package com.owlexpress.hub.domain.repository;
 
-import com.owlexpress.hub.domain.entity.HubIntervalInfo;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public interface HubIntervalInfoRepository {
-    HubIntervalInfo save(HubIntervalInfo intervalInfo);
+    void save(
+            UUID uuid,
+            UUID hubId,
+            UUID hubId1,
+            double distance,
+            Duration duration,
+            LocalDateTime parse
+    );
 }
