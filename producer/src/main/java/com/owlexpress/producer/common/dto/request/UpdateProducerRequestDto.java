317d1cb852e2884fc2875b419a71edf675696c61
Sunro1994 <gwangjulsr@gmail.com>
2025-03-20 05:08:29 +0900

    [OV-128] refactor(producer) : feignClient 통신 Dto 구현

--- producer/src/main/java/com/owlexpress/producer/common/dto/request/UpdateProducerRequestDto.java
+++ producer/src/main/java/com/owlexpress/producer/common/dto/request/UpdateProducerRequestDto.java
@@ -1,37 +1,32 @@
 package com.owlexpress.producer.common.dto.request;
 
 import com.owlexpress.producer.domain.entity.constant.CompanyType;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
-public class UpdateProductRequestDto {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UpdateProducerRequestDto {
 
 
     @Size(min = 1, max = 20)
-    @NotNull(message = "사업자 번호는 필수값입니다.")
     private String businessNumber;
     @Size(min = 1, max = 20)
-    @NotNull(message = "회사명은 필수값입니다.")
     private String companyName;
-    @NotNull(message = "회사 타입값은 필수값입니다.")
     private CompanyType companyType;
     @Size(min = 1, max = 255)
-    @NotNull(message = "회사 주소값은 필수값입니다.")
     private String companyAddress;
-    @NotNull(message = "위도 값은 필수입니다.")
     private Double latitude;
-    @NotNull(message = "경도 값은 필수입니다.")
     private Double longitude;
-    @NotNull(message = "허브Id값은 필수값입니다.")
     private UUID hubId;
 
     @Builder
-    public UpdateProductRequestDto(
+    public UpdateProducerRequestDto(
             String businessNumber,
             String companyName,
             CompanyType companyType,
