49a684cad2c95d1f67e29898a511f5444eeb5359
Sunro1994 <gwangjulsr@gmail.com>
2025-03-20 05:07:24 +0900

    [OV-128] refactor(producer) : feign Exception 핸들러 구현 및 예외 처리 메세지 상수화

--- producer/src/main/java/com/owlexpress/producer/common/exception/ProducerException.java
+++ producer/src/main/java/com/owlexpress/producer/common/exception/ProducerException.java
@@ -2,6 +2,12 @@ package com.owlexpress.producer.common.exception;
 
 public class ProducerException extends RuntimeException {
 
+    public static class NotAuthorizedException extends RuntimeException{
+        public NotAuthorizedException(String message) {
+            super(message);
+        }
+    }
+
     public static class ProducerNotFoundException extends RuntimeException{
         public ProducerNotFoundException(String message) {
             super(message);
