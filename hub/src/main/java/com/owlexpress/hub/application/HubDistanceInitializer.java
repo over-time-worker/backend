c53f85c610bd15c1d810bb4897f06dc5a8eb4ae6
Sunro1994 <gwangjulsr@gmail.com>
2025-03-20 00:44:18 +0900

    [OV-146] feat(hubIntervalInfo) : 최적 경로 계산 기능 구현
    - 다 익스트라 알고리즘 적용
    - P2P + Hub-spoke 방식 적용
    - Fresh 응급 배송은 아직 구현 안

--- hub/src/main/java/com/owlexpress/hub/application/HubDistanceInitializer.java
+++ hub/src/main/java/com/owlexpress/hub/application/HubDistanceInitializer.java
@@ -1,8 +1,6 @@
 package com.owlexpress.hub.application;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,9 +8,11 @@ import org.springframework.stereotype.Component;
 public class HubDistanceInitializer {
 
     private final HubDistanceService hubDistanceService;
-//서버에서 한 번만실행되는 TaskChecker Entity가 필요할지도...?
-    @EventListener(ApplicationReadyEvent.class)
-    public void initialize() {
-        hubDistanceService.calculateAllHubDistances();
-    }
+
+    //TODO:: 컨버터 해제하고 직접 코드 작성으로 수정해봄 잘 돌아나가 확인 필수!
+    // 이유 = 컨버터 인풋시에는 Long으로 인풋하나 반환시에는 다른타입으로 받아야함 (Entity가 Duration이므로)
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void initialize() {
+//        hubDistanceService.calculateAllHubDistances();
+//    }
 }
