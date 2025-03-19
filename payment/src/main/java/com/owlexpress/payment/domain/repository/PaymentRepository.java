68a8b5730397360f5a0bbde1058037fa8fffcd79
cnxw4570123 <cnxw4570123@gmail.com>
2025-03-19 16:40:54 +0900

    [OV-143] feat(payment) : 결제 상세 조회 기능 추가
    - FeignClient 연결 대비 DTO 클래스 추가
    - 임시로 mockData 반환하도록 설정

--- payment/src/main/java/com/owlexpress/payment/domain/repository/PaymentRepository.java
+++ payment/src/main/java/com/owlexpress/payment/domain/repository/PaymentRepository.java
@@ -2,10 +2,13 @@ package com.owlexpress.payment.domain.repository;
 
 import com.owlexpress.payment.domain.entity.Payment;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface PaymentRepository {
 
     Payment save(Payment payment);
 
     Optional<Payment> findByTransactionId(String transactionId);
+
+    Optional<Payment> findByOrderId(UUID orderId);
 }
