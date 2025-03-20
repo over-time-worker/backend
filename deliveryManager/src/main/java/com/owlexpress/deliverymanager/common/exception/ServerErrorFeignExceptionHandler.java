f9e479fb3b414addb74fa32b00ccdbca424137e6
Sunro1994 <gwangjulsr@gmail.com>
2025-03-20 17:55:55 +0900

    [OV-165] feat(deliveryManager) :  ExceptionHandler 구현

---
+++ deliveryManager/src/main/java/com/owlexpress/deliverymanager/common/exception/ServerErrorFeignExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.owlexpress.deliverymanager.common.exception;
+
+import com.owlexpress.deliverymanager.infrastructure.CommonDto;
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServerErrorFeignExceptionHandler implements FeignExceptionHandlerStrategy {
+
+    @Override
+    public boolean supports(FeignException e) {
+        return e.status() >=500;
+    }
+
+    @Override
+    public ResponseEntity<CommonDto<Void>> handleException(FeignException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                CommonDto.<Void>builder()
+                         .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                         .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                         .message("서버 오류가 발생했습니다: " + e.getMessage())
+                         .data(null)
+                         .build()
+        );
+    }
+}
