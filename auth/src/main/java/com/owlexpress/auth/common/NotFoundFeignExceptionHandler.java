1cdf4e55283666f42fdc783ec400593c725f5aa7
cnxw4570123 <cnxw4570123@gmail.com>
2025-03-21 11:55:54 +0900

    [OV-172] feat(auth) : feignClient 관련 예외 처리 설정

---
+++ auth/src/main/java/com/owlexpress/auth/common/NotFoundFeignExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.owlexpress.auth.common;
+
+import com.owlexpress.auth.presentation.dto.CommonDto;
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotFoundFeignExceptionHandler implements FeignExceptionHandlerStrategy {
+
+    @Override
+    public boolean supports(FeignException e) {
+        return e.status() == HttpStatus.NOT_FOUND.value();
+    }
+
+    @Override
+    public ResponseEntity<CommonDto<Void>> handleException(FeignException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                CommonDto.<Void>builder()
+                        .status(HttpStatus.NOT_FOUND)
+                        .code(HttpStatus.NOT_FOUND.value())
+                        .message("해당 데이터를 찾을 수 없습니다: " + e.getMessage())
+                        .data(null)
+                        .build()
+        );
+    }
+}
