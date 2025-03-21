d80b43a31a2e7b41fc1986d55b1a6837a2421f81
ggumi030 <tndus50843@gmail.com>
2025-03-21 16:16:13 +0900

    [OV-75] feat(alarm/ai/delivery) : commit reset (#241)

--- auth/src/main/java/com/owlexpress/auth/presentation/AuthController.java
+++ auth/src/main/java/com/owlexpress/auth/presentation/AuthController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,10 @@ public class AuthController {
     // TODO : 리프레시 토큰도 같이 발급.
 
     @PostMapping("/sign-in")
-    public ResponseEntity<TokenResponseDto> signIn(LoginRequestDto requestDto, HttpServletResponse response) {
+    public ResponseEntity<TokenResponseDto> signIn(
+            @RequestBody LoginRequestDto requestDto,
+            HttpServletResponse response
+    ) {
         response.setHeader("Authorization", authService.signIn(requestDto.toUserLoginRequestDto()));
 
         return ResponseEntity.ok().build();
