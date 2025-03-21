0c9f71067f028543dfdfebe850a91c793dde8873
ggumi030 <tndus50843@gmail.com>
2025-03-21 16:38:40 +0900

    [OV-75] feat(alarm/ai/delivery) : AI passport 적용 (#241)

--- ai/src/main/java/com/owl_express/ai/presentation/AiController.java
+++ ai/src/main/java/com/owl_express/ai/presentation/AiController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,9 +35,11 @@ public class AiController {
 
     @PostMapping("/messages/hub")
     public ResponseEntity<CommonDto<MessageCreateResponseDto>> createMessagesForHubDeliver(
-            @RequestBody HubDeliverMessageCreateRequestDto hubDeliverMessageCreateRequestDto
+            @RequestBody HubDeliverMessageCreateRequestDto hubDeliverMessageCreateRequestDto,
+            @RequestHeader("X-User-Passport") String passport
     ) {
-        MessageCreateResponseDto responseDto = aiService.createMessageForHubDeliver(hubDeliverMessageCreateRequestDto);
+        MessageCreateResponseDto responseDto
+                = aiService.createMessageForHubDeliver(hubDeliverMessageCreateRequestDto, passport);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 CommonDto.<MessageCreateResponseDto>builder()
@@ -49,9 +52,11 @@ public class AiController {
 
     @PostMapping("/messages/company")
     public ResponseEntity<CommonDto<MessageCreateResponseDto>> createMessagesForCompanyDeliver(
-            @RequestBody CompanyDeliverMessageCreateRequestDto companyDeliverMessageCreateRequestDto
+            @RequestBody CompanyDeliverMessageCreateRequestDto companyDeliverMessageCreateRequestDto,
+            @RequestHeader("X-User-Passport") String passport
     ) {
-        MessageCreateResponseDto responseDto = aiService.createMessageForCompanyDeliver(companyDeliverMessageCreateRequestDto);
+        MessageCreateResponseDto responseDto
+                = aiService.createMessageForCompanyDeliver(companyDeliverMessageCreateRequestDto, passport);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 CommonDto.<MessageCreateResponseDto>builder()
