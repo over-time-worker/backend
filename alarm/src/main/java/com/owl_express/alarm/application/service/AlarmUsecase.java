bc0a334d70cfab71172621ddd0bf4082f7d3cad4
ggumi030 <tndus50843@gmail.com>
2025-03-21 23:34:09 +0900

    [OV-181] feat(alarm/ai) : feignClient Header 추가, AI message delete 추가 (#250)

--- alarm/src/main/java/com/owl_express/alarm/application/service/AlarmUsecase.java
+++ alarm/src/main/java/com/owl_express/alarm/application/service/AlarmUsecase.java
@@ -1,7 +1,6 @@
 package com.owl_express.alarm.application.service;
 
 import static com.owl_express.alarm.common.exception.ExceptionMessage.MESSAGE_CREATE_FAIL_MESSAGE;
-import static com.owl_express.alarm.common.exception.ExceptionMessage.RETRY_MESSAGE;
 
 import com.owl_express.alarm.application.dtos.CommonDto;
 import com.owl_express.alarm.application.dtos.request.AlarmCreateRequestDto;
@@ -10,6 +9,7 @@ import com.owl_express.alarm.application.dtos.request.HubDeliverMessageCreateReq
 import com.owl_express.alarm.application.dtos.response.MessageCreateResponseDto;
 import com.owl_express.alarm.application.exceptions.AlarmException.AiFeignClientException;
 import com.owl_express.alarm.infrastructure.feignClient.AiClient;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +19,12 @@ public class AlarmUsecase {
 
     private final AiClient aiClient;
 
-    public MessageCreateResponseDto getHubDeliverMessageFromAi(AlarmCreateRequestDto requestDto) {
+    public MessageCreateResponseDto getHubDeliverMessageFromAi(AlarmCreateRequestDto requestDto, String passport) {
         MessageCreateResponseDto messageCreateResponseDto;
         HubDeliverMessageCreateRequestDto hubDeliverMessageCreateRequestDto = HubDeliverMessageCreateRequestDto.alarmDtoToMessageDto(requestDto);
 
             CommonDto<MessageCreateResponseDto> responseEntity
-                    = aiClient.createMessagesForHubDeliver(hubDeliverMessageCreateRequestDto);
+                    = aiClient.createMessagesForHubDeliver(hubDeliverMessageCreateRequestDto, passport);
 
             messageCreateResponseDto = responseEntity.getData();
 
@@ -35,12 +35,11 @@ public class AlarmUsecase {
         return messageCreateResponseDto;
     }
 
-    public MessageCreateResponseDto getCompanyDeliverMessageFromAi(AlarmCreateRequestDto requestDto) {
+    public MessageCreateResponseDto getCompanyDeliverMessageFromAi(AlarmCreateRequestDto requestDto, String passport) {
         MessageCreateResponseDto messageCreateResponseDto;
 
             CommonDto<MessageCreateResponseDto> responseEntity
-                    = aiClient.createMessagesForCompanyDeliver(
-                    CompanyDeliverMessageCreateRequestDto.alarmDtoToMessageDto(requestDto));
+                    = aiClient.createMessagesForCompanyDeliver(CompanyDeliverMessageCreateRequestDto.alarmDtoToMessageDto(requestDto), passport);
 
             messageCreateResponseDto = responseEntity.getData();
 
@@ -49,6 +48,10 @@ public class AlarmUsecase {
         }
 
         return messageCreateResponseDto;
+    }
+
+    public void deleteMessageToAi(UUID aiId, String passport) {
+        aiClient.delete(aiId, passport);
     }
 
 }
