a92f9c8f0e8d7fc4d2231ea4e01cbaf3d7ebb48c
cnxw4570123 <cnxw4570123@gmail.com>
2025-03-23 03:13:17 +0900

    refactor(payment) : 응답값으로 deliveryId 추가

--- payment/src/main/java/com/owlexpress/payment/infrastructure/client/DeliveryClient.java
+++ payment/src/main/java/com/owlexpress/payment/infrastructure/client/DeliveryClient.java
@@ -2,6 +2,8 @@ package com.owlexpress.payment.infrastructure.client;
 
 import com.owlexpress.payment.application.dto.request.DeliveryCreateRequestDto;
 import com.owlexpress.payment.presentation.dto.CommonDto;
+import java.util.Map;
+import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 public interface DeliveryClient {
 
     @PostMapping("/api/delivery")
-    CommonDto<Void> createDelivery(
+    CommonDto<Map<String, UUID>> createDelivery(
             @RequestBody DeliveryCreateRequestDto createRequestDto,
             @RequestHeader("X-User-Passport") String passport
     );
