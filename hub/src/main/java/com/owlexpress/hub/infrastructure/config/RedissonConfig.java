74f564e0b133d134870285c3e8bf583419d6cd12
ggumi030 <tndus50843@gmail.com>
2025-03-24 00:12:44 +0900

    [OV-213] feat/ redis config 설정 (#326)

--- hub/src/main/java/com/owlexpress/hub/infrastructure/config/RedissonConfig.java
+++ hub/src/main/java/com/owlexpress/hub/infrastructure/config/RedissonConfig.java
@@ -12,17 +12,6 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.CacheKeyPrefix;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.serializer.RedisSerializer;
-
-import java.time.Duration;
-
-import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
 
 @EnableCaching
 @Configuration
