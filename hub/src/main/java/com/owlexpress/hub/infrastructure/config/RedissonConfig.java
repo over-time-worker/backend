ff08e7b309e85c0ae8c41a305e56f306185ccc6f
cnxw4570123 <cnxw4570123@gmail.com>
2025-03-24 22:52:20 +0900

    [OV-215] refactor(hub) : 분산락 재시도 로직 추가

--- hub/src/main/java/com/owlexpress/hub/infrastructure/config/RedissonConfig.java
+++ hub/src/main/java/com/owlexpress/hub/infrastructure/config/RedissonConfig.java
@@ -8,10 +8,12 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.spring.cache.CacheConfig;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.springframework.boot.autoconfigure.cache.CacheProperties.Redis;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @EnableCaching
 @Configuration
