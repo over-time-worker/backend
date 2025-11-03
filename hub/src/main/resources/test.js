96c2053a49c9c7cbbfd460c0a561c7359a08e2d6
cnxw4570123 <cnxw4570123@gmail.com>
2025-11-04 00:10:38 +0900

    refactor(hub) : grafana + prometheus 연동

--- hub/src/main/resources/test.js
+++ hub/src/main/resources/test.js
@@ -1,14 +1,13 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
-import { SharedArray } from 'k6/data';
-import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import {check, sleep} from 'k6';
+import {uuidv4} from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
 // 테스트 설정
 export const options = {
   scenarios: {
     limited_requests: {
       executor: 'shared-iterations',
-      vus: 100,              // 동시에 100명의 가상 사용자가 실행됨
+      vus: 1000,              // 동시에 100명의 가상 사용자가 실행됨
       iterations: 1000,      // 총 1000회의 요청만 실행
       maxDuration: '1m30s',    // 1분30초 내에 테스트가 끝나도록 설정 (옵션)
     },
@@ -30,41 +29,42 @@ const ORDER_PAYLOAD = {
   "longitude": 127.987654,
   "orderProducts": [
     {
-      "productId": "a31c337f-d9c9-4ab3-8820-6a901a9d5cd0",
+      "productId": "efc50122-10d5-4bf8-8ee1-7ba60fcd20c6",
       "quantity": 1
     },
     {
-      "productId": "03e0ee67-1f14-47b7-9f9c-0a8c5e9497a3",
+      "productId": "8737dc22-482e-4ebe-b870-72818e7adf4e",
       "quantity": 1
     },
     {
-      "productId": "9d12d108-1368-4065-9b59-2768f87dcc45",
+      "productId": "566e78da-0ea2-4d44-bf38-6ec273562bdf",
       "quantity": 1
     }
   ]
 };
 const headers = {
   'Content-Type': 'application/json',
-  'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiYXV0aCI6Ik1BU1RFUiIsImlzcyI6Ik92TyIsImlhdCI6MTc0MzI0NzY0MiwiZXhwIjoxNzQzODUyNDQyfQ.EMflSs_h58eGlks_kSomugD8X0_QUVQCO6O8_2HtO72WNq4rfrSZ0Riky7OAXTgIm_N1DKpM0vn42d-m1do3OQ'
+  'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiYXV0aCI6Ik1BU1RFUiIsImlzcyI6Ik92TyIsImlhdCI6MTc2MjE2Mjk4OSwiZXhwIjoxNzYyNzY3Nzg5fQ.tbHn6-VfSE_BwsAz-vHoHVLSLbnHoVawc2jMgMwt-cETjy2q-ELSEtGvEvVA8V1kqypyuQKdIqzUmRqaB1eefw'
 };
 
-
-export default function() {  
+export default function () {
   // POST 요청 발송
   const response = http.post(
       `${BASE_URL}/hub/product/confirm-stock`,
       JSON.stringify(ORDER_PAYLOAD),
-      { headers: headers }
+      {headers: headers}
   );
-  
+
   // 응답 확인 (확장된 검증)
   check(response, {
     'status is 202': (r) => r.status === 202,
     'response body has data': (r) => r.json().hasOwnProperty('data'),
-    'products array exists': (r) => r.json().data.products && Array.isArray(r.json().data.products),
-    'expected product count': (r) => r.json().data.products.length === ORDER_PAYLOAD.orderProducts.length
+    'products array exists': (r) => r.json().data.products && Array.isArray(
+        r.json().data.products),
+    'expected product count': (r) => r.json().data.products.length
+        === ORDER_PAYLOAD.orderProducts.length
   });
-  
+
   // 응답 데이터 로깅 (선택사항)
   if (response.status !== 202) {
     console.log(`Failed response: ${response.body}`);
